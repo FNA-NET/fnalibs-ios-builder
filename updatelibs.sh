dd0df18115db039e38a6d36fcc33ac709867a603
ryancheung <ryancheung.go@gmail.com>
2021-09-30 06:40:50 +0800

    FNA-NET build

--- updatelibs.sh
+++ updatelibs.sh
@@ -16,14 +16,14 @@ fi
 
 if [ ! -d "./FNA3D/" ]; then
 	echo "FNA3D folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/FNA3D.git --recursive
+	git clone https://github.com/FNA-NET/FNA3D.git --recursive
 
 	echo ""
 fi
 
 if [ ! -d "./FAudio/" ]; then
 	echo "FAudio folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/FAudio.git
+	git clone https://github.com/FNA-NET/FAudio.git
 
 	echo ""
 fi
@@ -37,7 +37,7 @@ fi
 
 # Check for updates...
 echo "Updating SDL2..."
-cd SDL2 && git pull && cd ..
+cd SDL2 && git checkout release-2.0.16 && cd ..
 
 # Apply the IOS_DYLIB=1 patch for convenience
 echo ""
@@ -46,7 +46,7 @@ cd SDL2
 # Reset in case local changes made the patch incompatible
 git reset --hard
 # Apply patch
-git apply ../sdl2_fna_ios.patch
+git apply ../sdl2_fna_net_ios.patch
 cd ..
 
 echo ""
